2e0396ca954bc3685324bfad7ee077f3815ad07d
Abderrahmane <121558424+ELMETRAB@users.noreply.github.com>
2024-09-13 14:49:13 +0200

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -10,12 +10,14 @@ command = [
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 result = sock.connect_ex(('127.0.0.1',22))
 if result == 0:
-   print "Le port est ouvert"
+   print ("Le port est ouvert")
+   print ("N'oubliez pas de fermez le port 22 après utilisatopn du honeypot !")
 	SSHhoneypot()
 else:
    print "Port is not open"
    subprocess.run(command, shell=True, check=True)
    print("Le port 22 est maintenant ouvert.")
+   print ("N'oubliez pas de fermez le port 22 après utilisatopn du honeypot !")
    SSHhoneypot()
 	
 sock.close()
